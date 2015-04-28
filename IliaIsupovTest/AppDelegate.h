47db0198ad8aaead117929e1741b67c00fa7f349
Ilia Isupov <swinx89@gmail.com>
2015-04-28 15:41:38 +0300

    Initial Commit

---
+++ IliaIsupovTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  IliaIsupovTest
+//
+//  Created by Ilia Isupov on 28.04.15.
+//  Copyright (c) 2015 Test. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ IliaIsupovTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  IliaIsupovTest
+//
+//  Created by Ilia Isupov on 28.04.15.
+//  Copyright (c) 2015 Test. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
